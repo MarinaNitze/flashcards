43a83e17b4d515758114b3cdae7d39501a06d3ce
Marina Martin <git@marinamartin.com>
2013-12-22 20:46:32 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flashcards::Application.config.secret_key_base = 'e06075d7da0aeea27a8c80e93c4bd150a3a400fa2635d9be778513b3df46c3447adfa6b768edde0e3923e43070c3f1fa092549ac201a9aea1bea7d182378373f'
